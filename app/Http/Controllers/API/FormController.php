19a45bb18ef3f664bd88774f94f597e1705a0627
Hairul Lana <hairullana99@gmail.com>
2022-02-08 13:31:22 +0800

    update, delete data from api

--- app/Http/Controllers/API/FormController.php
+++ app/Http/Controllers/API/FormController.php
@@ -38,4 +38,42 @@ class FormController extends Controller
             'message' => 'success'
         ], 200);
     }
+
+    public function edit($id){
+        $student = Student::find($id);
+        
+        return response()->json([
+            'message' => 'success',
+            'data' => $student
+        ], 200);
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required'
+        ]);
+
+        $student = Student::find($id);
+
+        $student->update([
+            'name' => $request->name,
+            'address' => $request->address,
+            'phone' => $request->phone
+        ]);
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $student
+        ], 200);
+    }
+
+    public function delete($id){
+        Student::find($id)->delete();
+
+        return response()->json([
+            'message' => 'success delete'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,9 @@ use App\Http\Controllers\API\FormController;
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
   Route::post('/create', [FormController::class, 'create']);
+  Route::get('/edit/{id}', [FormController::class, 'edit']);
+  Route::post('/edit/{id}', [FormController::class, 'update']);
+  Route::post('/delete/{id}', [FormController::class, 'delete']);
   Route::get('/logout', [FormController::class, 'logout']);
 });
 
